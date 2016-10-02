5da513661ee97a422d9835a99ab6b65c38c50c36
Rudolf Fiala <afpdev@mogozine.com>
2016-10-02 21:58:15 +0200

    bugfix: method writeAFP() was not implemented correctly. Code cleanup.

--- src/main/java/com/mgz/afp/modca/MGO_MapGraphicsObject.java
+++ src/main/java/com/mgz/afp/modca/MGO_MapGraphicsObject.java
@@ -18,11 +18,13 @@ along with Alpheus AFP Parser.  If not, see <http://www.gnu.org/licenses/>
 */
 package com.mgz.afp.modca;
 
+import com.mgz.afp.base.IRepeatingGroup;
 import com.mgz.afp.base.RepeatingGroupWithTriplets;
 import com.mgz.afp.base.StructuredFieldBaseRepeatingGroups;
 import com.mgz.afp.exceptions.AFPParserException;
 import com.mgz.afp.parser.AFPParserConfiguration;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
@@ -34,20 +36,21 @@ public class MGO_MapGraphicsObject extends StructuredFieldBaseRepeatingGroups {
 
   @Override
   public void decodeAFP(byte[] sfData, int offset, int length, AFPParserConfiguration config) throws AFPParserException {
-    MGO_RepeatinGroup rg = new MGO_RepeatinGroup();
+    MGO_RepeatingGroup rg = new MGO_RepeatingGroup();
     rg.decodeAFP(sfData, offset, length, config);
     addRepeatingGroup(rg);
   }
 
-
   @Override
-  public void writeAFP(OutputStream os, AFPParserConfiguration config)
-          throws IOException {
-    // TODO Auto-generated method stub
-
+  public void writeAFP(OutputStream os, AFPParserConfiguration config)throws IOException {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    for(IRepeatingGroup rg : this.getRepeatingGroups()){
+      rg.writeAFP(baos,config);
+    }
+    super.writeFullStructuredField(os,baos.toByteArray());
   }
 
 
-  public static class MGO_RepeatinGroup extends RepeatingGroupWithTriplets {
+  public static class MGO_RepeatingGroup extends RepeatingGroupWithTriplets {
   }
 }
